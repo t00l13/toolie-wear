9e5aec396f65850f147e5431849e2d5cde7066c8
t00l13 <itollik@yandex.ru>
2022-08-03 00:39:16 +0300

    fix route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter, Route, Routes } from "react-router-dom";
 import Login from "./Components/Login/Login.js";
 import Register from "./Components/Register/Register.js";
 import Reset from "./Components/Reset/Reset.js";
@@ -9,7 +9,7 @@ import Welcome from "./Components/Welcome/Welcome";
 function App() {
   return (
     <div className="app">
-      <Router>
+      <HashRouter>
         <Routes>
           <Route exact path="/" element={<Welcome />} />
           <Route exact path="/login" element={<Login />}></Route>
@@ -17,7 +17,7 @@ function App() {
           <Route exact path="/reset" element={<Reset />} />
           <Route exact path="/home" element={<Home />} />
         </Routes>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
